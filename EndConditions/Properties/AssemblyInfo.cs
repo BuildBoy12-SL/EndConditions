23e83f60257124252dd6ff7bbeae28588c268a9d
BuildBoy12 <buildboy12345@gmail.com>
2020-07-14 18:47:06 -0400

    Update source
    Included in latest release, missed it on the drag and drop.

--- EndConditions/Properties/AssemblyInfo.cs
+++ EndConditions/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("EndConditions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("BuildBoy12")]
 [assembly: AssemblyProduct("EndConditions")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("9280B8BA-6290-4FC8-A58F-BC24E43AA486")]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
