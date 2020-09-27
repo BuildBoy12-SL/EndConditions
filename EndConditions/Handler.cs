6e8eadd8151b6e3d116dbd1bf020e7026fc6c529
BuildBoy12 <buildboy12345@gmail.com>
2020-09-26 20:53:58 -0400

    General fixes and patches
    + Fixed escape checks not refreshing
    + Various code changes
    + Update to Exiled 2.1.6

--- EndConditions/Handler.cs
+++ EndConditions/Handler.cs
@@ -1,49 +1,46 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Exiled.API.Features;
-using Exiled.Events.EventArgs;
-using Exiled.API.Enums;
-
 namespace EndConditions
 {
+	using Exiled.API.Enums;
+	using Exiled.API.Features;
+	using Exiled.Events.EventArgs;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	
 	public class Handler
-	{
-		private Plugin plugin;
-		public Handler(Plugin plugin) => this.plugin = plugin;
-
-		private bool AllowDebug => plugin.Config.AllowDebug;
-
-		internal static Dictionary<string, List<string>> EndConditions
+	{ 
+		internal static readonly Dictionary<string, List<string>> EndConditions
 			= new Dictionary<string, List<string>>();
-
-		private readonly Dictionary<string, bool> EscAdditions = new Dictionary<string, bool>
-		{
-			{ "-classd", RoundSummary.escaped_ds == 0 },
-			{ "+classd", RoundSummary.escaped_ds != 0 },
-			{ "-science", RoundSummary.escaped_scientists == 0 },
-			{ "+science", RoundSummary.escaped_scientists != 0 },
-		}; 
 		
 		public void OnCheckRoundEnd(EndingRoundEventArgs ev) 
 		{
-			if (!plugin.Config.AllowDefaultEndConditions) 
+			if (!Plugin.Instance.Config.AllowDefaultEndConditions) 
 			{
 				ev.IsAllowed = false;
 				ev.IsRoundEnded = false;
 			}
 			//Check if the warhead has detonated && if the detonation winner is set
-			if (Warhead.IsDetonated && plugin.Config.DetonationWinner != "none") 
+			if (Warhead.IsDetonated && Plugin.Instance.Config.DetonationWinner != "none") 
 			{ 
-				EndGame(ev, plugin.Config.DetonationWinner); 
+				EndGame(ev, Plugin.Instance.Config.DetonationWinner); 
 			}
 			else 
 			{
+				Dictionary<string, bool> escAdditions = new Dictionary<string, bool>
+				{
+					{ "-classd", RoundSummary.escaped_ds == 0 },
+					{ "+classd", RoundSummary.escaped_ds != 0 },
+					{ "-science", RoundSummary.escaped_scientists == 0 },
+					{ "+science", RoundSummary.escaped_scientists != 0 },
+				};
+				
 				//Shove all alive roles into the list
 				List<string> list = new List<string>();
 				foreach (Player ply in Player.List)
 				{
-					if (ply.Role.Equals(RoleType.Spectator)) continue;
+					if (ply.Role.Equals(RoleType.Spectator)) 
+						continue;
+					
 					if (ply.ReferenceHub.serverRoles.GetUncoloredRoleString().Contains("SCP-035"))
 					{
 						list.Add("scp035");
@@ -53,7 +50,7 @@ namespace EndConditions
 					list.Add(ply.Role.ToString().ToLower());
 				}
 				//If ignoretut, do the thing.
-				if (plugin.Config.IgnoreTutorials)
+				if (Plugin.Instance.Config.IgnoreTutorials)
 					list.RemoveAll(item => item == "tutorial");
 				//Put all the lists from the core dictionary and check em
 				foreach (var condition in EndConditions.Where(condition => !list.Except(condition.Value).Any()))
@@ -62,18 +59,18 @@ namespace EndConditions
 					{
 						//Get the key that contains the name and escape conditions
 						string key = EndConditions.FirstOrDefault(x => x.Value == condition.Value).Key;
-						Log.Debug($"Using conditions from condition name: '{key}'", AllowDebug);
+						Log.Debug($"Using conditions from condition name: '{key}'", Plugin.Instance.Config.AllowDebug);
 						//Check for escape conditions
 						string[] splitKey = key.Split(' ');
-						List<string> conds = splitKey.Where(str => EscAdditions.Keys.Contains(str)).ToList();
-						List<string> failedConds = conds.Where(x => !EscAdditions[x]).ToList();
+						List<string> conds = splitKey.Where(str => escAdditions.Keys.Contains(str)).ToList();
+						List<string> failedConds = conds.Where(x => !escAdditions[x]).ToList();
 						if (failedConds.Count > 0)
 						{
-							Log.Debug($"Failed at: {string.Join(", ", failedConds)}", AllowDebug);
+							Log.Debug($"Failed at: {string.Join(", ", failedConds)}", Plugin.Instance.Config.AllowDebug);
 							continue;
 						}
 
-						Log.Debug("Escape checks passed, ending round.", AllowDebug);
+						Log.Debug("Escape checks passed, ending round.", Plugin.Instance.Config.AllowDebug);
 						EndGame(ev, key);
 						return;
 					}
@@ -91,12 +88,13 @@ namespace EndConditions
 			ev.LeadingTeam = (LeadingTeam)ConvertTeam(team);
 			ev.IsRoundEnded = true;
 			ev.IsAllowed = true;
-			if (plugin.Config.AllowVerbose)
+			//_forceLock = true;
+			if (Plugin.Instance.Config.AllowVerbose)
 				Log.Info($"Force ending with {ev.LeadingTeam} as the lead team.");
 		}
 
 		//Middle man to convert string into RoundSummary.LeadingTeam to account for dictionary keys.
-		private int ConvertTeam(string arg) 
+		private static int ConvertTeam(string arg) 
 		{
 			var team = arg.ToLower().StartsWith("facilityforces") ? 0
 				: arg.ToLower().StartsWith("chaosinsurgency") ? 1
@@ -104,12 +102,11 @@ namespace EndConditions
 				: arg.ToLower().StartsWith("draw") ? 3
 				: -1;
 
-			if (team == -1)
-            {
-				Log.Debug($"Could not parse {arg} into a team, returning as a draw.", AllowDebug);
-				return 3;
-			}
-			return team;
+			if (team != -1) 
+				return team;
+			
+			Log.Debug($"Could not parse {arg} into a team, returning as a draw.", Plugin.Instance.Config.AllowDebug);
+			return 3;
 		}
 	}
 }
