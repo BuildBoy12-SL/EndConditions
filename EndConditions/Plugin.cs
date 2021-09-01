cb4dfb6d4058a8087d01d099ba59113ac54164f7
GameKuchen <49527417+GameKuchen@users.noreply.github.com>
2021-09-01 23:37:33 +0200

    Changed Version number

--- EndConditions/Plugin.cs
+++ EndConditions/Plugin.cs
@@ -40,7 +40,7 @@ namespace EndConditions
         public static Plugin Instance { get; } = InstanceValue;
 
         /// <inheritdoc/>
-        public override Version RequiredExiledVersion { get; } = new Version(2, 10, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
 
         /// <inheritdoc/>
         public override void OnEnabled()
